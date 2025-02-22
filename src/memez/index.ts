e4ca95f8bc93cb542e4c67d2d7b9eaa10a5e55ce
Jose Cerqueira <josecerqueira@protonmail.com>
2025-02-22 01:51:02 +0000

    :wrench: fix: type exports

--- src/memez/index.ts
+++ src/memez/index.ts
@@ -1,9 +1,12 @@
+import * as PumpTypes from './types/pump.types';
+import * as StableTypes from './types/stable.types';
+
 export * from './acl';
 export * from './constants';
 export * from './pump';
 export * from './stable';
 export * from './structs';
 export * from './types/memez.types';
-export * from './types/pump.types';
-export * from './types/stable.types';
 export * from './utils';
+
+export type { PumpTypes, StableTypes };
