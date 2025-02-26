c203144fff0bd8594625b489a5a72bc47e7d5923
Jose Cerqueira <josecerqueira@protonmail.com>
2025-02-26 04:57:59 +0000

    :wrench: fix: get-pool-metadata

--- src/scripts/memez/get-pool-metadata.ts
+++ src/scripts/memez/get-pool-metadata.ts
@@ -1,13 +1,13 @@
 import { log, memezPumpTestnet, TEST_POOL_ID } from '../utils.script';
 
 (async () => {
+  const pool = await memezPumpTestnet.getPumpPool(TEST_POOL_ID);
+
   const r = await memezPumpTestnet.getPoolMetadata({
     poolId: TEST_POOL_ID,
-    quoteCoinType: '0x2::sui::SUI',
-    memeCoinType:
-      '0xccba2a10e21adb177c0cd252049d774db4943d5acdd43904131ef7958c73d86a::meme::MEME',
-    curveType:
-      '0x63fed690a1154cfc4b31658443227de047cf3d305179aa5836e177c9efa57854::memez_pump::Pump',
+    quoteCoinType: pool.quoteCoinType,
+    memeCoinType: pool.memeCoinType,
+    curveType: pool.curveType,
   });
 
   log(r);
